19b499064ed565820394defbf2940db21e923efc
Mika Greif <mika.greif@greenbone.net>
2022-05-27 11:29:41 +0200

    Add nextjs project and nginx configuration

---
+++ scan-management/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: "/scan-management"
+}
+
+module.exports = nextConfig
---
+++ scan-management/pages/test/index.tsx
@@ -0,0 +1,5 @@
+function TestComponent () {
+    return <span>NEXT TEST</span>
+}
+
+export default TestComponent
